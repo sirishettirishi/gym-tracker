1fe16b99f5d6ef97aace382d73bf5c601bdf1fcc
sirishettirishi <sirishettirishi96@gmail.com>
2026-06-06 10:49:20 +0530

    Fixed session using localStorage

--- src/main/resources/static/session.js
+++ src/main/resources/static/session.js
@@ -1,37 +1,23 @@
-// Session and Cookie Management
 const SessionManager = {
-    set(name, value, days = 1) {
-        let expires = "";
-        if (days) {
-            let date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=Lax";
+    set(name, value) {
+        localStorage.setItem(name, value);
     },
 
     get(name) {
-        let nameEQ = name + "=";
-        let ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
+        return localStorage.getItem(name);
     },
 
     erase(name) {
-        document.cookie = name + '=; Max-Age=-99999999; path=/; SameSite=Lax';
+        localStorage.removeItem(name);
     },
 
     isAuthenticated() {
-        return this.get("userId") !== null;
+        return localStorage.getItem("userId") !== null;
     },
 
     logout() {
-        this.erase("userId");
-        this.erase("username");
+        localStorage.removeItem("userId");
+        localStorage.removeItem("username");
         window.location.href = "index.html";
     }
 };

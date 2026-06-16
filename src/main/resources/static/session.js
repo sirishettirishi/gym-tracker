1446d6fd817e0c2ab039132857ff64d22c1b4b42
sirishettirishi <sirishettirishi96@gmail.com>
2026-06-16 12:33:48 +0530

    Fixed session storage

--- src/main/resources/static/session.js
+++ src/main/resources/static/session.js
@@ -2,19 +2,15 @@ const SessionManager = {
     set(name, value) {
         sessionStorage.setItem(name, value);
     },
-
     get(name) {
         return sessionStorage.getItem(name);
     },
-
     erase(name) {
         sessionStorage.removeItem(name);
     },
-
     isAuthenticated() {
         return sessionStorage.getItem("userId") !== null;
     },
-
     logout() {
         sessionStorage.clear();
         window.location.href = "index.html";
